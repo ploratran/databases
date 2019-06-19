4f4fd781020daad1f6240144fdd9fd2a99948ea9
ploratran <ploratran@gmail.com>
2019-06-19 16:53:58 -0700

    create tables messages and users

--- server/schema.sql
+++ server/schema.sql
@@ -4,10 +4,17 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT PRIMARY KEY,
+  user TEXT,
+  content TEXT,
+  room TEXT 
 );
 
 /* Create other tables and define schemas for them here! */
-
+CREATE TABLE users (
+  id INT PRIMARY KEY,
+  username TEXT 
+);
 
 
 
